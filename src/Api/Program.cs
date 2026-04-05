1b963bdc15378048505c082ea1cb41f735782722
Parag <parag.raut@outlook.com>
2026-04-05 15:13:16 +0200

    Refactor project structure and upgrade to .NET 10.0; update dependencies and modify API endpoints

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,11 +1,10 @@
 using ComicsProvider;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddOpenApi();
 
 builder.Services.AddComicsService();
 
@@ -14,8 +13,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
